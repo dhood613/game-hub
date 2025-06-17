140d7fb71d028a07e4d158a4648cb0327d0d0c59
David Hood <dhood613@gmail.com>
2025-06-17 17:11:16 -0400

    Cleanup Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,13 @@
 import useGenres from "@/hooks/useGenres";
 import type { Genre } from "@/interfaces/Genre";
-import { Image, VStack, HStack, Spinner, Button } from "@chakra-ui/react";
+import {
+  Image,
+  VStack,
+  HStack,
+  Spinner,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
@@ -12,21 +19,32 @@ export const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (err) return null;
   if (isLoading || !data || !Array.isArray(data)) return <Spinner></Spinner>;
   return (
-    <VStack align="start" gap={3} paddingY="5px">
-      {data.map((genre) => (
-        <HStack key={genre.id} paddingX="5px">
-          <Image src={genre.image_background} alt={genre.name} boxSize="32px" />
-          <Button
-            fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
-            onClick={() => onSelectGenre(genre)}
-            fontSize="sm"
-            paddingY="5px"
-            variant={"plain"}
-          >
-            {genre.name}
-          </Button>
-        </HStack>
-      ))}
-    </VStack>
+    <>
+      <Heading fontSize="2xl" paddingBottom="10px">
+        Genres
+      </Heading>
+      <VStack align="start" gap={3} paddingY="5px">
+        {data.map((genre) => (
+          <HStack key={genre.id}>
+            <Image
+              src={genre.image_background}
+              alt={genre.name}
+              boxSize="32px"
+              objectFit="cover"
+            />
+            <Button
+              whiteSpace={"normal"}
+              textAlign="left"
+              fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
+              onClick={() => onSelectGenre(genre)}
+              paddingY="5px"
+              variant={"plain"}
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        ))}
+      </VStack>
+    </>
   );
 };
